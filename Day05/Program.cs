4be8c2142ce306441fc19b392705196635bda546
Christoffer Illum Søndergaard <sircas@sircas.dk>
2018-10-23 18:03:18 +0200

    Add solution for day 05

---
+++ Day05/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-------------------------+");
+            Console.WriteLine("| Advent of Code - Day 05 |");
+            Console.WriteLine("+-------------------------+");
+
+            Part1();
+            Part2();
+
+            Console.WriteLine($" - Glædelig jul");
+        }
+
+        private static void Part1()
+        {
+            var input = File.ReadAllLines("input.1.txt").Select(x => int.Parse(x)).ToList();
+
+            int sp = 0;
+            int steps = 0;
+
+            while (sp < input.Count && sp > -1)
+            {
+                var delta = input[sp];
+                ++input[sp];
+                sp += delta;
+                ++steps;
+            }
+
+            Console.WriteLine($"Part1: Used {steps} steps before out of range");
+        }
+
+        private static void Part2()
+        {
+            var input = File.ReadAllLines("input.1.txt").Select(x => int.Parse(x)).ToList();
+
+            int sp = 0;
+            int steps = 0;
+
+            while (sp < input.Count && sp > -1)
+            {
+                var delta = input[sp];
+                
+                if(delta>= 3)
+                {
+                    --input[sp];
+                } else {
+                    ++input[sp];
+                }
+
+                sp += delta;
+                ++steps;
+            }
+
+            Console.WriteLine($"Part2: Used {steps} steps before out of range");
+        }
+    }
+}
